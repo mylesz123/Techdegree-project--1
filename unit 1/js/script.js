6ba02bdc1637d0e808b4b6c76799d9fcc7184d6b
Myles Y <39877233+mylesz123@users.noreply.github.com>
2018-08-01 11:01:45 -0400

    converted myQuotes to JSON file

--- unit 1/js/script.js
+++ unit 1/js/script.js
@@ -1,35 +1,7 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-let myQuotes=
-[
-  {
-			quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
-			name:"Francis of Assisi"
-	    },
-	    {
-	    	quote:"Believe you can and you're halfway there.",
-	    	name:"Theodore Roosevelt"
-	    },
-	    {
-	    	quote:"It does not matter how slowly you go as long as you do not stop.",
-	    	name:"Confucius"
-	    },
-	    {
-	    	quote:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-	    	name:"Thomas A. Edison"
-	    },
-	    {
-	    	quote:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
-	    	name:"Confucius"
-	    },
-      {
-        quote:'If ya don\'t know now ya know',
-        name:'-The Notorious B.I.G',
-        citation:,
-        year: 1998
-  }
-];
+
 //random quote selector
 let randomQuote = Math.floor( Math.random() * (myQuotes.length));
 
@@ -42,7 +14,7 @@ $('#quote-box').html(myQuotes.randomQuote);
 
 // Create the printQuote funtion and name it printQuote
 function printQuote() {
-
+return getRandomQuote;
 };
 
 
