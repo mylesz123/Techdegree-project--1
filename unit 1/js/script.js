1a602eba1171723195cf139048504c63765e364d
Myles Y <39877233+mylesz123@users.noreply.github.com>
2018-08-16 10:17:46 -0400

    almost perfect!

--- unit 1/js/script.js
+++ unit 1/js/script.js
@@ -18,31 +18,31 @@ var colorWheel = [
 
 var quotes = [
   {
-			quo: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
-			name:"Francis of Assisi"
+			quo: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible. ",
+			name:"Francis of Assisi "
 	    },
 	    {
-	    	quo:"Believe you can and you're halfway there.",
-	    	name:"Theodore Roosevelt",
+	    	quo:"Believe you can and you're halfway there. ",
+	    	name:"Theodore Roosevelt ",
         year:'1942'
 	    },
 	    {
-	    	quo:"It does not matter how slowly you go as long as you do not stop.",
+	    	quo:"It does not matter how slowly you go as long as you do not stop. ",
 	    	name:"Confucius"
 	    },
 	    {
-	    	quo:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-	    	name:"Thomas A. Edison",
+	    	quo:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. ",
+	    	name:"Thomas A. Edison ",
         citation:'The Edison Files'
 	    },
 	    {
-	    	quo:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
+	    	quo:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence. ",
 	    	name:"Confucius"
 	    },
       {
-        quo:'If ya don\'t know now ya know',
-        name:'-The Notorious B.I.G',
-        citation: 'A song I heard on the radio',
+        quo:'If ya don\'t know now ya know ',
+        name:'The Notorious B.I.G ',
+        citation: 'A song I heard on the radio ',
         year: '1998'
   }
 ];
@@ -59,6 +59,7 @@ function getRandomQuote(quotes) {
 function getRandomColor(){
   var randomColor = Math.floor(Math.random() * colorWheel.length);
   var screenColor = colorWheel[randomColor];
+  return(screenColor);
   //getRandomQuote();
 };
 
@@ -67,33 +68,34 @@ function getRandomColor(){
 // Create the print Quote funtion and name it printQuote
 function printQuote(){
   var getFunc = getRandomQuote(quotes); //random quote selector
-  var getColor = getRandomColor(colorWheel)
   var quoteString = `<p class="quote"> ${getFunc.quo} </p><p class="name"> ${getFunc.name} </p>`;
-
-function $swapColor(getColor){
-  $("body").css({ backgroundColor: colorWheel[getColor]});
-  return getColor;
-  };
+  var getColor = getRandomColor(colorWheel)
 
     //need conditional statement for citation and year for span element
 
-      if(getFunc.hasOwnProperty("citation") || getFunc.hasOwnProperty("year")) { //(quotes === citation || quotes === year )
-
-        $('#quote-box').html(quoteString +=  `<span class="citation"> ${getFunc.citation} </span><span class="year"> ${getFunc.year} </span>`);
-
-        }
-
-      else
-    {
-        $('#quote-box').html(quoteString);
-        $swapColor();
-        //quoteString;//when i log quotes, an index number shows in the console
-    }
-        //return(getFunc);
-
+    if(getFunc.hasOwnProperty('citation')) {
+      $('#quote-box').html(quoteString += `<span class="citation"> ${getFunc.citation} </span>`);
+      $("body").css({ backgroundColor: getColor}); // trying to use .css property to change background color {document.body.style.backgroundColor = getColor;
+      $("#loadQuote").css({ backgroundColor: getColor});
+ }
+    else if(getFunc.hasOwnProperty('year')) {
+      $('#quote-box').html(quoteString += `<span class="year"> ${getFunc.year} </span>`);
+      $("body").css({ backgroundColor: getColor});
+      $("#loadQuote").css({ backgroundColor: getColor});
+ }
+    else {
+      $('#quote-box').html(quoteString);
+      $("body").css({ backgroundColor: getColor});
+      $("#loadQuote").css({ backgroundColor: getColor});
+      //quoteString;//when i log quotes, an index number shows in the console
+}
+};
+function printAuto(){
+myVar = setInterval(printQuote, 7500);
 };
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 printQuote();
+printAuto();
