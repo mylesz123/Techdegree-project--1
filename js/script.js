eab5745c53ffa20f0f391fb2b349401cbd07b2e4
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-25 23:47:36 -0500

    update functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-var colorWheel = [
+const colorWheel = [
   "#16a085",
-   "#27ae60",
-   "#2c3e50",
-   "#f39c12",
-   "#e74c3c",
-   "#9b59b6",
-   "#FB6964",
-   "#342224",
-   "#472E32",
-   "#BDBB99",
-   "#77B1A9",
-   "#73A857"
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857"
 ];
 
-var quotes = [
+const quotes = [
   {
-			quo: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible. ",
-			name:"Francis of Assisi "
+  			quo: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible. ",
+  			name:"Francis of Assisi "
 	    },
 	    {
 	    	quo:"Believe you can and you're halfway there. ",
@@ -42,7 +42,7 @@ var quotes = [
       {
         quo:'If ya don\'t know now ya know ',
         name:'The Notorious B.I.G ',
-        citation: 'A song I heard on the radio ',
+        citation: 'A song I heard on the radio',
         year: '1998'
   }
 ];
@@ -50,56 +50,46 @@ var quotes = [
 // Create the getRandomQuuote function and name it getRandomQuote
 // takes 1 parameter which is the array
 
-function getRandomQuote(quotes) {
-  var randomNumber = Math.floor(Math.random() * (quotes.length));
-  var randomQuote = quotes[randomNumber];  //this allows the quotes to be randomized
-  //add logic
-    return randomQuote;
+function getRandomQuote(array) {
+  return quotes[Math.floor(Math.random() * array.length)];
 };
 
 function getRandomColor(){
-  var randomColor = Math.floor(Math.random() * colorWheel.length);
-  var screenColor = colorWheel[randomColor];
-  return(screenColor);
-  //getRandomQuote();
+  return colorWheel[Math.floor(Math.random() * colorWheel.length)];
 };
 
 // Create the print Quote funtion and name it printQuote
 function printQuote(){
-  var getFunc = getRandomQuote(quotes); //random quote selector
-  var quoteString = `<p class="quote"> ${getFunc.quo} </p><p class="name"> ${getFunc.name} `;
-  var getColor = getRandomColor(colorWheel)
+  let getFunc = getRandomQuote(quotes); //random quote selector
+  let quoteString = `<p class="quote"> ${getFunc.quo} </p><p class="name"> ${getFunc.name} `;
+  let getColor = getRandomColor(colorWheel);
 
     //need conditional statement for citation and year for span element
-
     if(getFunc.hasOwnProperty('citation')) {
       $('#quote-box').html(quoteString += `<span class="citation"> ${getFunc.citation} </span></p>`);
       $("body").css({ backgroundColor: getColor}); // trying to use .css property to change background color {document.body.style.backgroundColor = getColor;
       $("#loadQuote").css({ backgroundColor: getColor});
- }
-     if(getFunc.hasOwnProperty('year')) {
+    }
+    if(getFunc.hasOwnProperty('year')) {
       $('#quote-box').html(quoteString += `<span class="year"> ${getFunc.year} </span></p>`);
       $("body").css({ backgroundColor: getColor});
       $("#loadQuote").css({ backgroundColor: getColor});
- }
-
-      $('#quote-box').html(quoteString += `</p>`);
-      $("body").css({ backgroundColor: getColor});
-      $("#loadQuote").css({ backgroundColor: getColor});
-      //quoteString;//when i log quotes, an index number shows in the console
-
+    }
+    //close p tag
+    $('#quote-box').html(quoteString += `</p>`);
+    $("body").css({ backgroundColor: getColor});
+    $("#loadQuote").css({ backgroundColor: getColor});
 };
+
+const loadQuote = document.getElementById('loadQuote');
 
 function printAuto(){ //to set how long a quote stays before the next appears
-
-$('#loadQuote').on('click', () => {
-  myVar = setInterval(getRandomQuote, 5000);
-  myVar.getRandomQuote;
-})
+  loadQuote.addEventListener("click", function() {
+    setInterval(printQuote, 5500);
+    $('#loadQuote').hide();
+  });
 };
-
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+loadQuote.addEventListener("click", printQuote, false);
 printAuto();
-printQuote();
